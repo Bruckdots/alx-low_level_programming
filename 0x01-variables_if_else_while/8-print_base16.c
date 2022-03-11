c39f580ca354876fb7c1617d5a11b4d6a32993e6
Bruck Yazew <99271880+bruckswrld@users.noreply.github.com>
2022-03-11 01:40:43 -0800

    printing all numbers of base 16, in lower,case followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- * Description: Prints the alphabet in lowercase.
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-  char nody;
-
-  for (nody = 48 ; nody < 58 ; nody++)
-    {
-      putchar(nody);
-    }
-  for (nody = 97 ; nody < 103 ; nody++)
-    {
-      putchar(nody);
-    }
-  putchar('\n');
-
-  return (0);
-}0;136;0c#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
